4f7171045f1242e1b7d06d5b5d6227e7e35a5284
schalchlincode <schalchlincode@gmail.com>
2023-05-17 08:59:39 -0500

    add mood and sickness buttons.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function updateHunger() {
 
   if (hunger <= 0) {
     hungerElement.textContent = 'YOUUU KILLLEDD MEEEEE';
-    document.getElementById('increaseBtn').disabled = true;
+    document.getElementById('increaseHungerBtn').disabled = true;
     return;
   }
 
@@ -23,8 +23,8 @@ function increaseHunger(amount) {
 document.addEventListener('DOMContentLoaded', (event) => {
   setInterval(updateHunger, 100);
 
-  const increaseBtn = document.getElementById('increaseBtn');
-  increaseBtn.addEventListener('click', () => {
+  const increaseHungerBtn = document.getElementById('increaseHungerBtn');
+  increaseHungerBtn.addEventListener('click', () => {
     increaseHunger(10);
   });
 });
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 //Bathroom
 
 let bathroom = 2000;
-const decreaseRate = 1;
+const decreaseBathroomRate = 1;
 
 function updateBathroom() {
   const bathroomElement = document.getElementById('Bathroom');
@@ -41,11 +41,11 @@ function updateBathroom() {
 
   if (bathroom <= 0) {
     bathroomElement.textContent = 'YOUUU KILLLEDD MEEEEE';
-    document.getElementById('increaseBtn1').disabled = true;
+    document.getElementById('increaseBathroomBtn').disabled = true;
     return;
   }
 
-  bathroom -= decreaseRate;
+  bathroom -= decreaseBathroomRate;
 }
 
 function increaseBathroom(amount) {
@@ -55,8 +55,71 @@ function increaseBathroom(amount) {
 document.addEventListener('DOMContentLoaded', (event) => {
   setInterval(updateBathroom, 100);
 
-  const increaseBtn1 = document.getElementById('increaseBtn1');
-  increaseBtn1.addEventListener('click', () => {
+  const increaseBathroomBtn = document.getElementById('increaseBathroomBtn');
+  increaseBathroomBtn.addEventListener('click', () => {
     increaseBathroom(10);
+  });
+});
+
+
+//Mood
+
+let mood = 2000;
+const decreaseMoodRate = 1;
+
+function updateMood() {
+  const moodElement = document.getElementById('Mood');
+  moodElement.textContent = `Mood: ${mood}`;
+
+  if (mood <= 0) {
+    moodElement.textContent = 'YOUUU KILLLEDD MEEEEE';
+    document.getElementById('increaseMoodBtn').disabled = true;
+    return;
+  }
+
+  mood -= decreaseMoodRate;
+}
+
+function increaseMood(amount) {
+  mood += amount;
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  setInterval(updateMood, 100);
+
+  const increaseMoodBtn = document.getElementById('increaseMoodBtn');
+  increaseMoodBtn.addEventListener('click', () => {
+    increaseMood(10);
+  });
+});
+
+//sickness
+
+let sickness = 2000;
+const decreaseSicknessRate = 1;
+
+function updateSickness() {
+  const sicknessElement = document.getElementById('Sickness');
+  sicknessElement.textContent = `Sickness: ${sickness}`;
+
+  if (sickness <= 0) {
+    sicknessElement.textContent = 'YOUUU KILLLEDD MEEEEE';
+    document.getElementById('increaseHungerBtn').disabled = true;
+    return;
+  }
+
+  sickness -= decreaseSicknessRate;
+}
+
+function increaseSickness(amount) {
+  sickness += amount;
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  setInterval(updateSickness, 100);
+
+  const increaseSicknessBtn = document.getElementById('increaseSicknessBtn');
+  increaseSicknessBtn.addEventListener('click', () => {
+    increaseSickness(10);
   });
 });
